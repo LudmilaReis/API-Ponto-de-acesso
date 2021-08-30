293c63f29610e3ed426d34960fd8670aa08c54c8
Ludmila Reis <ludmila.creiis@gmail.com>
2021-08-30 19:14:28 -0300

    Second commit

---
+++ src/main/java/com/github/ludmilareis/live/repository/JornadaRepository.java
@@ -0,0 +1,9 @@
+package com.github.ludmilareis.live.repository;
+
+import com.github.ludmilareis.live.model.JornadaTrabalho;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface JornadaRepository extends JpaRepository<JornadaTrabalho, Long> {
+}
